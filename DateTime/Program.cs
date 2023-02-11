aa39d1f56e1a30d6f698bbb9b7bbb9434abcef06
Victork102 <Victork102@hotmail.com>
2023-02-11 17:13:07 -0300

    1

---
+++ DateTime/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Date Time
+            DateTime data = new DateTime(2023, 02, 11);
+            DateTime dataHora = new DateTime(2023, 02, 11, 22, 05, 33);
+
+
+            Console.WriteLine(data.ToString());
+            Console.WriteLine(dataHora.ToString());
+
+            Console.WriteLine("");
+
+            //Para acessar cada campo individualmente
+
+            Console.WriteLine("O ano é: " + dataHora.Year);
+            Console.WriteLine("O mês é: " + dataHora.Month);
+            Console.WriteLine("O dia é: " + dataHora.Day);
+            Console.WriteLine("O horário é: " + dataHora.Hour +" horas,"+ dataHora.Minute +" minutos e "+ dataHora.Second + " segundos." );
+            Console.WriteLine("Esse dia foi uma " + dataHora.DayOfWeek + ", e foi o " + dataHora.DayOfYear + "° dia do ano.");
+
+            Console.WriteLine("");
+            //Data e Hora Atual
+
+
+            DateTime dataHoraNow = DateTime.Now;
+
+
+
+            Console.WriteLine("A data e hora atual é: "+dataHoraNow);
+
+            Console.WriteLine("");
+            // String to Datetime
+
+            DateTime dataConv = Convert.ToDateTime("22/11/2030 14:10:25");
+
+            Console.WriteLine(dataConv);
+            Console.WriteLine("O ano é: " + dataConv.Year);
+            Console.WriteLine("O mês é: " + dataConv.Month);
+            Console.WriteLine("O dia é: " + dataConv.Day);
+            Console.WriteLine("O horário é: " + dataConv.Hour + " horas," + dataConv.Minute + " minutos e " + dataConv.Second + " segundos.");
+            Console.WriteLine("Esse dia foi uma " + dataConv.DayOfWeek + ", e foi o " + dataConv.DayOfYear + "° dia do ano.");
+
+            //
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey(true);
+        }
+    }
+}
--- Operadores Matematicos Avançados/Program.cs
+++ Operadores Matematicos Avançados/Program.cs
@@ -11,20 +11,88 @@ namespace Operadores_Matematicos_Avançados
         static void Main(string[] args)
         {
 
-            // Potenciação
+            // Potenciação = .Pow(Numero,Potência)
 
             double potenciacao = Math.Pow(4, 2);
-            Console.Write(potenciacao);
+            Console.WriteLine("Potenciação 4²: "+potenciacao);
 
-            // Radiciação
+            // Radiciação = .Pow(Número,1/'raiz');
 
             double raiz = 3;
-            double radiciacao = 3;
+            double radiciacao = Math.Pow(27, 1 / raiz);
+
+            Console.WriteLine("Radiciação √27:" + radiciacao);
+
+            // Maximo e Minimo .Max(num1, num2) .Min(num1, num2)
+
+            double max = Math.Max(4, 8);
+            Console.WriteLine("Máximo entre 4 e 8: "+max);
+
+            double min = Math.Min(4, 8);
+            Console.WriteLine("Minimo entre 4 e 8: "+min);
+
+            // Modulo = .Abs(num)
+
+            double modulo = Math.Abs(-10);
+            Console.WriteLine("Módulo de -10: "+modulo);
+
+            //Operações Trigonométricas:
+
+            // Angulo em Radian = angulo * (2 * Math.PI) / 360
+            double angle = 30 * (2 * Math.PI) / 360;
+
+            // Seno = .Sin(num)
+            double seno = Math.Sin(angle);
+
+            Console.WriteLine("O seno de 30° é: "+seno);
+
+            // Coseno = .Cos(num)
+
+            double coseno = Math.Cos(angle);
+            Console.WriteLine("O coseno de 30° é: " + coseno);
 
 
+            // Tangente = .Tan(num)
 
+            double tan = Math.Tan(angle);
+            Console.WriteLine("A tangente de 30° é: " + tan);
 
+            // Arcseno = .Asin(num)
 
+            double Aseno = Math.Asin(seno);
+            double AsenoGraus = Aseno * 360 / (2 * Math.PI);
+            Console.WriteLine("O arcseno de "+seno+" é: " + Aseno+", que equivale a: "+AsenoGraus+"°");
+
+            // Arccoseno = .Acos(num)
+
+            double Acos = Math.Acos(coseno);
+            double AcosGraus = Acos * 360 / (2 * Math.PI);
+            Console.WriteLine("O arccoseno de " + coseno + " é: " + Acos + ", que equivale a: " + AcosGraus + "°");
+
+            // Arctangente = .Atan(num)
+
+            double Atan = Math.Atan(tan);
+            double AtanGraus = Atan * 360 / (2 * Math.PI);
+            Console.WriteLine("A arctangente de " + tan + " é: " + Atan + ", que equivale a: " + AtanGraus + "°");
+
+            // Arredondamento .Ceiling(num) .Floor(num)
+
+            double num1 = 3.33;
+            double arrenumup = Math.Ceiling(num1);
+
+            Console.WriteLine("O arredondamento para cima de " + num1 + " é: " + arrenumup);
+
+            double arrenumdown = Math.Floor(num1);
+
+            Console.WriteLine("O arredondamento para baixo de " + num1 + " é: " + arrenumdown);
+
+            // Logaritmo
+
+            double base10 = Math.Log10(100);
+            Console.WriteLine("O logaritmo de base 10 de 100 é: " + base10);
+
+            double baseE = Math.Log(Math.E * Math.E * Math.E * Math.E);
+            Console.WriteLine("O logaritmo de base E é: " + baseE);
 
             Console.ReadKey();
         }
