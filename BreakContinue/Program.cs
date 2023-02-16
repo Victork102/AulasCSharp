51f374ad8f41b5a2fd2f4de0448e04ab3d2f51e1
Victork102 <Victork102@hotmail.com>
2023-02-16 19:15:53 -0300

    1

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = 0;
+            while (true)
+            {
+                Console.WriteLine("Loop Count: " + loop);
+                Console.WriteLine("Pressione C para continuar");
+                Console.WriteLine("Pressione B para break");
+                loop++;
+
+                string tecla = Console.ReadKey(true).KeyChar.ToString();
+
+
+                if (tecla == "c")
+                {
+                    Console.Clear(); // Limpa o Console
+                    continue; // Continua o loop, seguindo para a próxima iteração
+                    
+                }
+                else if (tecla == "b")
+                {
+                    break; // encerra o loop
+                }
+                else
+                {
+                    Console.WriteLine("Tecla desconhecida");
+                }
+
+
+
+
+
+                Console.WriteLine("Pressione qualquer tecla para continuar");
+                Console.ReadKey(true);
+                Console.Clear();
+            }
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ProjetoFatorial/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                Console.WriteLine("Digite qualquer número para calcular o Fatorial.");
+                Console.WriteLine("Caso contrário, digite S para sair");
+
+                string opcao = Console.ReadLine();
+
+                if (opcao == "s" | opcao == "S")
+                {
+                    break;
+                }
+                else
+                {
+                    UInt64 valor = Convert.ToUInt64(opcao);
+                    if (valor == 0)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("O fatorial de 0 é 1.");
+
+                    }
+                    else if (valor < 0)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Não existe fatorial de números negativos");
+                    }
+                    else
+                    {
+                        UInt64 resultado = 1;
+
+                        Console.Write(valor + "! = ");
+                        for (UInt64 i = valor; i > 0; i--)
+                        {
+                            resultado *= i;
+                            if (i == 1)
+                            {
+                                Console.Write(i + " = " + resultado);
+
+                            }
+                            else
+                            {
+
+                                Console.Write(i + "x");
+
+                            }
+
+                        }
+
+
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("Pressione Qualquer tecla para continuar");
+                    Console.ReadKey(true);
+                    Console.Clear();
+
+                }
+
+
+
+
+
+               
+            }
+            Console.ReadKey(true);
+        } 
+        
+    }
+}
