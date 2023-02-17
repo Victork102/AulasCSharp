db0d8d8e4f20749fc3b2281d5fdc27defa0eb728
Victork102 <Victork102@hotmail.com>
2023-02-16 21:32:09 -0300

    1

---
+++ ManipulacaoStrings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulacaoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Indexação Caracter
+
+            string nome = "Guilherme";
+            char letra = nome[4];
+
+
+            Console.WriteLine(letra);
+
+
+            // Concatenação
+
+
+            string sobrenome = "Silva";
+
+            string nomeCompleto = nome + " " + sobrenome;
+
+            Console.WriteLine(nomeCompleto);
+            
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ProjetoLogin/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int attempt = 1;
+
+                while (attempt <= 3)
+                {
+
+
+                    {
+
+                        Console.WriteLine("Digite o Usuário:");
+
+                        string user = Console.ReadLine();
+
+                        Console.WriteLine("Digite a senha:");
+
+                        string senha = "";
+
+
+
+
+                        while (true)
+                        {
+
+                            ConsoleKeyInfo passInput = Console.ReadKey(true);
+
+                            if (passInput.Key == ConsoleKey.Enter)
+                            {
+
+                                break;
+                            }
+                            else
+                            {
+
+                                senha += passInput.KeyChar;
+
+                            }
+
+
+                        }
+
+                        if (user == "Valéria" && senha == "1234")
+                        {
+
+                            Console.WriteLine("Usiário Logado com sucesso!");
+                            break;
+                        }
+                        else
+                        {
+
+                            attempt++;
+                            Console.WriteLine("Usuário/Senha incorreto!");
+
+
+                        }
+
+
+
+
+
+                    }
+
+
+
+                }
+
+                Console.WriteLine("Número máximo de tentativas excedido!");
+                break;
+
+            }
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey(true);
+        }
+    } 
+}
