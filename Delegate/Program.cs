158d5368059d22f1539480450538d479ecd411fc
Victork102 <Victork102@hotmail.com>
2023-02-21 18:04:55 -0300

    1

---
+++ Delegate/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Delegate
+{
+	class Program
+	{
+
+		// Declarando Delegate: tipo_de_acesso delegate tipo_de_dado nome(Parâmetros);
+		public delegate double MeuDelegateOperacoes(double a, double b);
+		public static double Soma(double a, double b)
+		{
+			double resSoma = a + b;
+			Console.WriteLine("A soma de " + a + " mais " + b + " é igual a " + resSoma);
+			return resSoma;
+		}
+		public static double Multiplicar(double a, double b)
+		{
+			double resMulti = a * b;
+			Console.WriteLine("A multiplicação de " + a + " por " + b + " é igual a " + resMulti);
+			return resMulti;
+		}
+		public static double Divisao(double a, double b)
+		{
+			double resDiv = a / b;
+			Console.WriteLine("A divisão de " + a + " por " + b + " é igual a " + resDiv);
+			return resDiv;
+		}
+
+		public static void ExecutaOperação(MeuDelegateOperacoes minhasOperacoes)
+		{
+			Console.WriteLine("Executamdo Metodo passado como Parâmetro de Entrada.");
+			minhasOperacoes(50, 20);
+		}
+		static void Main(string[] args)
+		{
+
+			MeuDelegateOperacoes minhasOperacoes;
+
+			minhasOperacoes = Soma;
+
+			double resDelegate = minhasOperacoes(10, 20);
+
+			Console.WriteLine("O resultado do Delegate é: " + resDelegate);
+
+			Console.WriteLine("=============");
+
+			minhasOperacoes += Multiplicar;
+			minhasOperacoes += Divisao;
+
+			resDelegate = minhasOperacoes(30, 5);
+			Console.WriteLine("O resultado do Delegate é: " + resDelegate);
+
+			Console.WriteLine("=============================");
+
+			ExecutaOperação(Multiplicar);
+
+
+
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ ManipularArquivos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ManipularArquivos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			// Criando caminhos de arquivo
+
+			// método 1
+
+			string caminho1 = "D:\\C#\\Aulas\\teste1.txt";
+
+			// método 2
+
+			string caminho2 = @"D:\C#\Aulas\teste2.txt";
+
+			// Criar arquivo
+
+			FileStream meuArquivo1 = File.Create(caminho1);
+			meuArquivo1.Close();
+
+			FileStream meuArquivo2 = File.Create(caminho2);
+			meuArquivo2.Close();
+
+			// Verificar se arquivo existe
+
+			bool arquivo1 = File.Exists(caminho1);
+			bool arquivo2 = File.Exists(caminho2);
+
+
+			Console.WriteLine("Arquivo 1 existe? " + arquivo1 + " || Arquivo 2 existe: " + arquivo2);
+
+			// Deletando Arquivos
+
+			File.Delete(caminho1);
+
+			arquivo1 = File.Exists(caminho1);
+
+			Console.WriteLine("Arquivo 1 existe? " + arquivo1 + " || Arquivo 2 existe: " + arquivo2);
+
+
+			Console.ReadKey();
+		}
+	}
+}
