30e7b89c7d1c5d3fbba2c5b19e72402353a49c46
Victork102 <Victork102@hotmail.com>
2023-02-21 11:11:58 -0300

    1

---
+++ Metodos/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Metodos
+{
+
+
+
+
+    class Program
+    {
+
+        //Declarar método:  tipo_de_acesso modificador_de_instância tipo_de_retorno nome_do_metodo(parâmetros_de_entrada)
+
+        public static double Soma(double a, double b)
+        {
+            double resSoma = a + b;
+            return resSoma;
+            // alternativa: return(a+b);
+        }
+
+        public static void PrintMessage()
+        {
+            Console.WriteLine("Este é um Método vazio!");
+            Console.WriteLine("Ele não tem parâmetros de entrada ou saída");
+        }
+
+        // Modificadores de Parâmetros de Entrada
+
+        /*
+         * ref = parâmetro de entrada PODE ser modificado pelo Método
+         * out = parâmetro de entrada DEVE ser modificado pelo Método
+         */
+
+        public static void Multiplica(double a, double b, ref double resOperacao)
+        {
+            double resMulti = a * b;
+            resOperacao = resMulti;
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Chamando Métodos
+
+            string opcao;
+
+            while (true)
+            {
+
+                Console.WriteLine("C para calcular, M para Método, S para sair");
+                opcao = Convert.ToString(Console.ReadKey(true).KeyChar);
+                opcao = opcao.ToLower();
+                if (opcao == "c")
+                {
+                    Console.WriteLine("Digite o primeiro número:");
+                    double a = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo número:");
+                    double b = Convert.ToDouble(Console.ReadLine());
+
+                    double res = Soma(a, b);
+                    Console.WriteLine(res);
+                    Console.ReadKey(true);
+                    Console.Clear();
+                    continue;
+                }
+                else if (opcao == "m")
+                {
+                    PrintMessage();
+
+                    Console.WriteLine("Digite o primeiro número:");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo número:");
+                    double d = Convert.ToDouble(Console.ReadLine());
+
+                    double resMulti  = 0;
+                    Multiplica(c, d, ref resMulti);
+                    Console.WriteLine(resMulti);
+
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else if (opcao == "s")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("opção Invalida!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+            }
+            
+            
+
+
+
+            
+        }
+    }
+}
---
+++ QuintoProjeto/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuintoProjeto
+{
+    class Program
+    {
+
+        public struct dadosCadastrais_Struct
+        {
+            public String nomeCompleto;
+            public DateTime dataNasc;
+            public String nomeRua;
+            public int numCasa;
+
+        }
+
+        public enum resultado_e
+        {
+            Sucesso = 0,
+            Sair = 1,
+            Excecao = 2
+        }
+
+        public static void PrintMessage(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            Console.WriteLine("Pressione qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static resultado_e PegaString(string minhaString, string mensagem)
+        {
+            resultado_e retorno;
+            Console.WriteLine(mensagem);
+            string temp = Console.ReadLine();
+            if (temp == "s" || temp == "S")
+            {
+                retorno = resultado_e.Sair;
+            }
+            else
+            {
+                minhaString = temp;
+                retorno = resultado_e.Sucesso;
+            }
+            Console.Clear();
+            return retorno;
+        }
+
+        public static resultado_e PegaData(ref DateTime data, string mensagem)
+		{
+            resultado_e retorno;
+            do
+            {
+                try
+                {
+                    Console.WriteLine(mensagem);
+                    string temp = Console.ReadLine();
+                    if (temp = "s" || temp = "S")
+					{
+                        retorno = resultado_e.Sair;
+					}
+					else
+					{
+                        data = Convert.ToDateTime(temp);
+                        retorno = resultado_e.Sucesso;
+					}
+                }
+                catch(Exception e)
+				{
+                    Console.WriteLine("EXCEÇÂO: " + e.Message);
+                    PrintMessage("Pressione qualquer tecla para continuar");
+                    retorno = resultado_e.Excecao;
+				}
+
+            } while (retorno==resultado_e.Excecao);
+            Console.Clear();
+            return retorno;
+		}
+
+        static void Main(string[] args)
+        {
+            List<dadosCadastrais_Struct> ListaUsuarios = new List<dadosCadastrais_Struct>();
+
+            string opcao = "";
+
+            do
+            {
+                Console.WriteLine("Digite C para cadastrar novo usuário ou digite S para sair");
+                opcao = Console.ReadKey(true).KeyChar.ToString().ToLower();
+
+                if (opcao == "c")
+                {
+                    // Cadastrar Usuário
+
+
+                }
+                else if (opcao == "S")
+                {
+                    //Sair
+                    PrintMessage("Encerrando Programa");
+                }
+                else
+                {
+                    //Opção Inválida
+                    PrintMessage("Opção Inválida");
+                }
+            } while (opcao != "s");
+        }
+    }
+}
