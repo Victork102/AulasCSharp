18a28b9140bcb44b199dafd9c52120d7f501597c
Victork102 <Victork102@hotmail.com>
2023-03-04 17:59:52 -0300

    1

---
+++ ThreadTimer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTimer
+{
+	class Program
+	{
+		// metodo executado pelo timer
+		static void TimerTick(object state)
+		{
+			Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+		}
+
+
+
+
+
+		static void Main(string[] args)
+		{
+			//criando timer
+			TimerCallback tcb = new TimerCallback(TimerTick);
+			Timer myTimer = new Timer(tcb, null, 0, 1000);
+			
+			Console.ReadKey();
+
+			// encerra timer
+			myTimer.Dispose();
+		}
+	}
+}
---
+++ Timers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Timers
+{
+	class Program
+	{
+
+		// função para evento timer
+
+		static void TimerTick(object sender, EventArgs e)
+		{
+			Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+		}
+
+
+		static void Main(string[] args)
+		{
+			// Criando Timer
+
+			Timer timer = new Timer(1000);
+			timer.AutoReset = true;
+			// Cadastra evento
+			timer.Elapsed += TimerTick;
+			// Inicializa Timer
+			timer.Start();
+
+
+
+
+			Console.ReadKey();
+			// Encerra timer
+			timer.Stop();
+		}
+	}
+}
